c51441e88684b94015f760ba0a0aaafc2a7b5eb0
Justin Peters <JMarvel525@gmail.com>
2022-08-19 13:32:38 -0700

    add swagger to API

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,15 +1,19 @@
-
+using System;   //Add for swagger documentation
+using System.Reflection; //add for swagger documentation
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO; //Adds definition for path, use to ad swagger documentation
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models; //add for swagger documentation
-using System;   //Add for swagger documentation
-using System.Reflection; //add for swagger documentation
 using AnimalShelter.Models;
-using System.IO; //Adds definition for path, use to ad swagger documentation
+
 namespace AnimalShelter
 {
     public class Startup
@@ -28,27 +32,9 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-
-            services.AddSwaggerGen(options =>
+            services.AddSwaggerGen(c=>
             {
-              options.SwaggerDoc("v1", new OpenApiInfo
-              {
-                Version = "v1",
-                Title = "AnimalShelter Api",
-                Description = "An ASP.NET Core Web API for displaying animals at an animal shelter",
-                Contact = new OpenApiContact
-                {
-                  Name = "Justin Peters", 
-                  Url = new Uri("https://github.com/jpeters25")
-                },
-                License = new OpenApiLicense
-                {
-                  Name = "MIT License",
-                  Url = new Uri("https://github.com/jpeters25/AnimalShelter.Solution/blob/main/LICENSE")
-                }
-              });
-              var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-              options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+              c.SwaggerDoc("v1", new OpenApiInfo {Title = "AnimalShelter", Version = "v1"});
             });
         }
 
@@ -58,6 +44,8 @@ namespace AnimalShelter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIBoilerPlate v1"));
             }
 
             // app.UseHttpsRedirection();
@@ -69,13 +57,6 @@ namespace AnimalShelter
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
-
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-              options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-              options.RoutePrefix = string.Empty;
             });
         }
     }
